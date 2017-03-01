2e9aa26f75b618c043cb7a163fe7aad7fe206dab
zhangyy <m_zhangyinyue@163.com>
2017-03-01 17:23:33 +0800

    20170301.1

---
+++ conf/settings.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""基于rabbitmq的简单ssh的通用参数."""
+
+# rabbitmq服务器地址
+hostname = '192.168.146.135'
+port = 5672
+username = 'test'
+passwd = 'test'
+vhost = '/'
+q_name = 'command'
---
+++ core/ssh_client_rpc.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""基于rabbitmq的简单ssh server端."""
+
+import pika
+import subprocess
+import settings
+
+
+class SSHClient(object):
+    """ssh server类，从rabbitmq服务器获取客户端命令、执行、返回结果到指定的queue."""
+
+    def __init__(self):
+        """初始化连接到rabbitmq服务器的参数."""
+        user_info = pika.PlainCredentials(
+            settings.username, settings.passwd)
+        conn_params = pika.ConnectionParameters(
+            settings.hostname, settings.port, settings.vhost, user_info)
+        self.queue_name = settings.q_name
+        self.connection = pika.BlockingConnection(conn_params)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(self.queue_name)
+        self._set_consume()
+
+    def _set_consume(self):
+        """设置接收消息的参数."""
+        self.channel.basic_consume(self._callback, self.queue)
+
+    def _callback(self, channel, method, properties, body):
+        """收到消息时的回调方法."""
+        command = body
+        result = self._exec_cmd(command)
+        routing_key = properties.reply_to
+        delivery = method.delivery_tag
+        self._put_result_to_rabbit(result, routing_key, properties)
+        self.channel.basic_ack(delivery_tag=delivery)
+
+    def _exec_cmd(self, cmd):
+        """执行命令."""
+        return subprocess.getoutput(cmd)
+
+    def _put_result_to_rabbit(self, message, routing_key, properties):
+        """将消息发送到rabbitmq."""
+        self.channel.basic_publish(
+            exchange='',
+            routing_key=routing_key,
+            body=message,
+            properties=properties
+        )
